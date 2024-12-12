fb238602c66fba0bb1bcd10a2f07014f235d579e
diogoantunes291 <diogoantunes291@gmail.com>
2024-12-12 08:59:58 +0000

    Add files via upload

---
+++ ITClinical_web_crawler/ITClinical_web_crawler/ITClinical_web_crawler/spiders/main.py
@@ -0,0 +1,43 @@
+import scrapy
+import csv
+
+
+class ITClinicalCrawler(scrapy.Spider):
+    name = 'itclinical'
+    start_urls = ["https://itclinical.com/it.php"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.csv_file = open("ITClinical_Data.csv", "w", newline="", encoding="utf-8")
+        self.csv_writer = csv.writer(self.csv_file)
+        self.csv_writer.writerow(["Title", "Feature"])
+
+    def closed(self, reason):
+        print(f"Closed: {reason}")
+        self.csv_file.close()
+
+    def parse(self, response, **kwargs):
+        print("Visited", response.url)
+        sections = response.xpath('//a[contains(@href, "it.php")]/following-sibling::ul/li/a/@href').getall()
+        print("Sections Found:", sections)
+        for section in sections:
+            yield response.follow(section, callback=self.parse_section)
+
+    def parse_section(self, response):
+        title = response.xpath('//h2/text()').get()
+        features = []
+
+        if title:
+            title = title.strip()
+            features = response.xpath(
+                '//div[@class="container"]//h3[@class="margin-reset"]/following-sibling::ul/li/text()').getall()
+        else:
+            print("Page not found")
+
+        print(title, "\n")
+        for feature in features:
+            feature = feature.strip()
+            print("    ", feature)
+            self.csv_writer.writerow([title, feature])
+
+        print("\n")
---
+++ ITClinical_web_crawler/README.txt
@@ -0,0 +1,3 @@
+Web crawler designed to gather information on the ITClinical website (https://itclinical.com/it.php) from their Software pages inside the "IT Affairs" sections (CTMS, EDC, IMPO, IWRS, etc) and collect the bulletpoints inside the "Features" section.
+
+After all the information gathered it will export it into a csv file.
